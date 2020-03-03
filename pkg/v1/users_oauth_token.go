de16fafa54e5cafe83b000db9676dc9f1c5adeed
zeromsi <www.zeromsi2@gmail.com>
2020-03-03 23:43:09 +0600

    Add save and findByUsername method of Users_Oauth_Token

--- pkg/v1/users_oauth_token.go
+++ pkg/v1/users_oauth_token.go
@@ -1,6 +1,10 @@
 package v1
 
-import "github.com/go-bongo/bongo"
+import (
+	"github.com/go-bongo/bongo"
+	"github.com/nonCriticInc/heimdall/config"
+	"gopkg.in/mgo.v2/bson"
+)
 
 type Users_Oauth_Token struct {
 	bongo.DocumentBase `bson:",inline"`
@@ -11,4 +15,23 @@ type Users_Oauth_Token struct {
 	RefreshToken       string `bson:"refreshToken"`
 	RefreshTokenIat    int64  `bson:"refreshTokenIat"`
 	Code               string `bson:"code"`
+}
+
+
+func (users_Oauth_Token *Users_Oauth_Token) Save() error{
+	err := config.UsersOauthTokenCollection.Save(users_Oauth_Token)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (users_Oauth_Token *Users_Oauth_Token) FindByUserId() Users_Oauth_Token{
+	query := bson.M{"$and": []bson.M{
+		{"userId": users_Oauth_Token.UserId},
+	},
+	}
+	temp:=Users_Oauth_Token{}
+	config.UsersOauthTokenCollection.Find(query).Query.One(&temp)
+	return temp
 }
